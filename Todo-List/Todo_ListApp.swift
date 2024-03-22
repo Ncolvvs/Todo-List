6b153943db5bf59d8277ef8cbbba546fbbe10382
Nicolas <nicolassantiago.5050@gmail.com>
2024-03-22 13:37:44 -0300

    Initial Commit

---
+++ Todo-List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo-List
+//
+//  Created by Nicolas Santiago on 22-03-24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
